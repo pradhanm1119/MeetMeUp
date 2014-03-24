def9036ff45c70638f5dd9ac1303e173e952df34
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-24 15:17:09 -0500

    third commit - code cleanup

--- MeetMeUp/MPRViewController.m
+++ MeetMeUp/MPRViewController.m
@@ -19,54 +19,18 @@
 
 - (void)viewDidLoad
 {
-//    NSURL *url            = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=2b35722b207036d151b4453654be73"];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    
     // Download data from MeetUp JSON file
     NSData *allMeetUpData = [[NSData alloc] initWithContentsOfURL:
-                              [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=2b35722b207036d151b4453654be73"]];
+                             [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=2b35722b207036d151b4453654be73"]];
     
     NSError *error;
     NSMutableDictionary *allMeetUps = [NSJSONSerialization
                                        JSONObjectWithData:allMeetUpData
                                        options:NSJSONReadingMutableContainers
                                        error:&error];
-    //[myTableView reloadData];
     
     allMeetUpsArray = allMeetUps[@"results"];
-    
-//    if(error)
-//    {
-//        NSLog(@"%@", [error localizedDescription]);
-//    }
-//    else {
-//        allMeetUpsArray = allMeetUps[@"results"];
-//        for (NSDictionary *theCourse in monday)
-//        {
-//            NSLog(@"----");
-//            NSLog(@"Title: %@", theCourse[@"title"] );
-//            NSLog(@"Speaker: %@", theCourse[@"speaker"] );
-//            NSLog(@"Time: %@", theCourse[@"time"] );
-//            NSLog(@"Room: %@", theCourse[@"room"] );
-//            NSLog(@"Details: %@", theCourse[@"details"] );
-//            NSLog(@"----");
-//        }
-//    }
-
-    
-    //    heroes = @[@{@"Name": @"Superman", @"Age": @32},
-    //               @{@"Name": @"Wonder Woman", @"Age": @23}];
-    
     [super viewDidLoad];
-    
-//    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^
-//     (NSURLResponse *response, NSData *data, NSError *connectionError)
-//     {
-//         NSError *error;
-//         myMeetUps = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
-//         [myTableView reloadData];
-//         
-//     }];
 }
 
 - (void)didReceiveMemoryWarning
@@ -84,12 +48,9 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MeetupID"];
     NSDictionary    *meetUp = allMeetUpsArray[indexPath.row];
     
-    //NSURL           *url = [NSURL URLWithString:superhero[@"avatar_url"]];
-    
     cell.textLabel.text       = meetUp[@"name"];
     cell.detailTextLabel.text = [meetUp[@"venue"] objectForKey:@"address_1"];
-    //cell.detailTextLabel.text = meetUp[@"venue", @"address"];
-    //cell.imageView.image      = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+    
     return cell;
 
 }
