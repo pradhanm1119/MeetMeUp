fd3a9c9110951c136c17c31c1c4baf439b8d177d
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-24 13:42:03 -0500

    Initial Commit

---
+++ MeetMeUp/MPRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPRAppDelegate.h
+//  MeetMeUp
+//
+//  Created by Manas Pradhan on 3/24/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeetMeUp/MPRViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPRViewController.h
+//  MeetMeUp
+//
+//  Created by Manas Pradhan on 3/24/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPRViewController : UIViewController
+
+@end
